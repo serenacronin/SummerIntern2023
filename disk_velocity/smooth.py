4560ffe3e34e59440eeebb9a2372b0feae674565
scronin <cronin.104@osu.edu>
2023-06-30 13:04:31 -0400

    adding plotting option

--- disk_velocity/smooth.py
+++ disk_velocity/smooth.py
@@ -14,6 +14,8 @@ fills in the average of the nearest neighbors.
 # ========================================================================================
 from astropy.convolution import Gaussian2DKernel, convolve
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.offsetbox import AnchoredText
 
 # ========================================================================================
 # DEFINE FUNCTIONS
@@ -53,36 +55,96 @@ def gauss_2d_kernel(sigma):
 # DEFINE VARIABLES
 # ========================================================================================
 
-beam =  'FIXME' # beamsize taken from the header
+beam =  'FIXME' # beamsize taken from the header (in arcsec)
 mask_kern = 'FIXME'*beam  # sigma for 2D Gaussian kernel to do the masking
 fill_kern = 'FIXME'*beam  # sigma for 2D Gaussian kernel to fill in masked values
 
-orig_cube == 'FIXME'  # open the original datacube
+orig_cube == 'FIXME'  # path to open the original datacube
 orig_data == 'FIXME'  # grab the data from the original datacube
+
+plot = False  # set to True if you want to plot the intermediate steps
+              # to help you figure out what beam to smooth to
 
 # ========================================================================================
 # SMOOTH THE MAPS
 # ========================================================================================
-
 kern_arr = gauss_2d_kernel(sigma=mask_kern)  # build the kernel
    
- # convolve with the original map; this will find pixels that deviate
- # a lot from their neighbors
+# convolve with the original map; this will find pixels that deviate
+# a lot from their neighbors
 new_image = convolve(orig_data, kern_arr, normalize_kernel=False, nan_treatment='fill')
+
+if plot == True:
+    vmin = 'FIXME: SET VELOCITY MIN FOR PLOTTING'
+    vmax = 'FIXME: SET VELOCITY MAX FOR PLOTTING'
+    ax = plt.subplot(2, 2, 1)
+    ax.imshow(orig_data, vmin=vmin, vmax=vmax, origin='lower', cmap='RdBu_r')
+    ax.set_title('Original', fontsize=20)
+    ax.tick_params(axis='both', which='both',direction='in',
+                   width=2.5, labelsize=16, length=7)
+    ax.set_xlabel('x pixels', fontsize=20)
+    ax.set_ylabel('y pixels', fontsize=20)
 
 # set a threshold: we want to find pixels that deviate by +/- 100
 # blank out these pixels; this is our mask!
-mask = np.abs(new_image) < 100
+mask = np.abs(new_image) < 100  # feel free to mess with this threshold!
 copy_im = orig_data.copy()
 copy_im[~mask] = np.nan
+
+if plot == True:
+    ax = plt.subplot(2, 2, 2)
+    im = ax.imshow(mask, origin='lower', cmap='Greys_r')
+    ax.set_title('Mask', fontsize=20)
+    ax.tick_params(axis='both', which='both',direction='in',
+                   width=2.5, labelsize=16, length=7)
+    ax.coords[0].set_auto_axislabel(False)
+    ax.coords[0].set_ticklabel_visible(False)
+    ax.coords[1].set_auto_axislabel(False)
+    ax.coords[1].set_ticklabel_visible(False)
+    
+    # add an annotation
+    at = AnchoredText(
+    '%sx beam' % int(round(mask_kern/beam,1)), prop=dict(size=18), frameon=True, loc='lower right')
+    at.patch.set_boxstyle("round,pad=0.,rounding_size=0.2")
+    ax.add_artist(at)
 
 # now we want to fill in these nan pixels with the average
 # of the nearest neighbors
 kernel = Gaussian2DKernel(fill_kern)
 final_image = convolve(copy_im, kernel)
 
+if plot == True:
+    ax = plt.subplot(2, 2, 3)
+    ax.imshow(final_image, vmin=-160, vmax=160, origin='lower',cmap='RdBu_r')
+    ax.set_title('Smoothed Image',fontsize=20)
+    ax.tick_params(axis='both', which='both',direction='in',
+                    width=2.5, labelsize=16, length=7)
+    
+    # add an annotation
+    at = AnchoredText(
+    '%sx beam' % int(round(fill_kern/beam,1)), prop=dict(size=18), frameon=True, loc='lower right')
+    at.patch.set_boxstyle("round,pad=0.,rounding_size=0.2")
+    ax.add_artist(at)
+
+    ax.coords[0].set_auto_axislabel(False)
+    ax.coords[0].set_ticklabel_visible(False)
+    ax.coords[1].set_auto_axislabel(False)
+    ax.coords[1].set_ticklabel_visible(False)
+
 # this adds some weird stuff to the edges, so lets blank the edges back out
 # so we have the original dimensions again
 final_image[np.isnan(orig_cube[1])] = np.nan # [0] has some nans within
+
+if plot == True:
+    ax.imshow(final_image, vmin=vmin, vmax=vmax, origin='lower',cmap='RdBu_r')
+    ax.set_title('Final Image',fontsize=20)
+    ax.tick_params(axis='both', which='both',direction='in',
+                    width=2.5, labelsize=16, length=7)
+    ax.coords[0].set_auto_axislabel(False)
+    ax.coords[0].set_ticklabel_visible(False)
+    ax.coords[1].set_auto_axislabel(False)
+    ax.coords[1].set_ticklabel_visible(False)
+
+    plt.show()
 
 ## FIXME: save the final image as a FITS file
